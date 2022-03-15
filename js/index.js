f738c0cc3a7b5f80b573601705d3889dbea1f88f
KaoruNishikawa <KaoruNishikawa@users.noreply.github.com>
2022-03-15 13:29:21 +0000

    deploy: cb343c834533d1ad87f603af48ebb5b70e0f32c2

--- js/index.js
+++ js/index.js
@@ -1,33 +1,29 @@
 "use strict"
 
-// const taggedElements = document.getElementsByClassName("dm-link")
-// const linkElem = document.createElement("i")
-// linkElem.classList.add("fa", "fa-hashtag")
-// linkElem.setAttribute("aria-hidden", "true")
-// for (let element of taggedElements) {
-//     linkElem.id = ""
-//     console.log(element)
-//     element.insertBefore(linkElem, element.firstChild)
-// }
-
-const rootURL = document.URL
+// const rootURL = document.URL
 
 const markdownRootPath = "./mdcontent/"
 const markdownPath = (title) => markdownRootPath + title + ".md"
+
+const anchorFAClass = "fa fa-hashtag"
+const anchorHTML = (link) => `<a class="anchor" href="#${link}"><i class="${anchorFAClass}"></i></a>`
 
 const markdownBox = document.getElementsByClassName("dm-md-content")
 for (let box of markdownBox) {
     const path = markdownPath(box.title)
     axios.get(path).then(
-        (response) => {
+        response => {
             const parsedHTML = marked.parse(response.data)
             box.innerHTML = DOMPurify.sanitize(parsedHTML)
         }
-    )
-    for (elem of box.children) {  // async??
-        elem.classList.add("dm-md-element")
-        if (elem.id !== "") {
-            elem.innerHTML += `<a class="anchor" href="#${box.title}"><i class="fa fa-hashtag"></i></a>`
+    ).then(
+        _ => {
+            for (elem of box.children) {
+                elem.classList.add("dm-md-element")
+                if (elem.id !== "") {
+                    elem.innerHTML += anchorHTML(box.title)
+                }
+            }
         }
-    }
+    )
 }
