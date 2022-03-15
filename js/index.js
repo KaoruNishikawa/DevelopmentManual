6713fb3e2c15f47c14b0422c96c379db1b0bdc54
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-03-15 17:48:08 +0900

    Use Markdown

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 "use strict"
 
-const taggedElements = document.getElementsByClassName("dm-link")
-const linkElem = document.createElement("i")
-linkElem.classList.add("fa", "fa-hashtag")
-linkElem.setAttribute("aria-hidden", "true")
-for (let element of taggedElements) {
-    linkElem.id = ""
-    element.insertBefore(linkElem, element.firstChild,)
+// const taggedElements = document.getElementsByClassName("dm-link")
+// const linkElem = document.createElement("i")
+// linkElem.classList.add("fa", "fa-hashtag")
+// linkElem.setAttribute("aria-hidden", "true")
+// for (let element of taggedElements) {
+//     linkElem.id = ""
+//     console.log(element)
+//     element.insertBefore(linkElem, element.firstChild)
+// }
+
+const markdownBox = document.getElementsByClassName("dm-md-content")
+for (let box of markdownBox) {
+    const path = "./" + box.title + ".md"
+    axios.get(path).then(
+        (response) => {
+            const parsedHTML = marked(response.data)
+            // parsedHTML.q
+            console.log(parsedHTML)
+            box.innerHTML = parsedHTML
+        }
+    )
 }
+axios.get()
